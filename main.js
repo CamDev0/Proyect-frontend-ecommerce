5d3b92e3c8d76ee08259ffcdcda429b9df2f19b9
Camilo Barrios <camilobarriosdev@gmail.com>
2022-10-02 20:20:10 -0500

    Agregados todos los eventos de JS, falta arreglar algunos detalles

--- main.js
+++ main.js
@@ -44,10 +44,97 @@ const cartModalContainer = document.querySelector('.cart-modal__checkout-contain
 cartIconBtn.addEventListener('click', () => {
     cartModal.classList.toggle('show');
 
-    if(lastValue > 0) {
+    if(lastValue == 0) {
+        cartModalContainer.innerHTML = '<p class = "cart-empty">Your cart is empty. </p>';
+    }
+    else {
         drawProductModal();
     }
+});
 
+
+// 5. Cambiar imagenes cuando se presione el boton flecha.
+
+const imageContainer = document.querySelector('.gallery__image-container');
+const nextBtn = document.querySelector('.gallery__next');
+const previousBtn = document.querySelector('.gallery__previous');
+let imgIndex = 1;
+
+nextBtn.addEventListener('click', () => {
+    changeNextImage(imageContainer);
+});
+
+previousBtn.addEventListener('click', () => {
+    changePreviousImage(imageContainer);
+});
+
+
+// 6. Mostrar el modal de imagenes cuando hago click en la imagen principal. DESKTOP
+
+const imageModal = document.querySelector('.modal-gallery__background');
+const closeModalIcon = document.querySelector('.modal-gallery__close')
+
+imageContainer.addEventListener ('click', () => {
+    imageModal.style.display = 'grid';
+});
+
+closeModalIcon.addEventListener('click', () => {
+    imageModal.style.display = 'none';
+});
+
+// ... Cambiar las imagenes principales en la portada dando click en ellas.
+
+//no-list
+let galleryThumnails = document.querySelectorAll('.gallery__thumnails'); 
+//lo convertimos a un array.
+galleryThumnails = [...galleryThumnails];
+
+galleryThumnails.forEach(thumnail =>{
+    thumnail.addEventListener('click' , event => {
+        imageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id}.jpg')`;
+    });
+});
+
+// ... Cambiar las imagenes principales de la portada en el MODAL.
+const modalImageContainer = document.querySelector('.modal-gallery__image-container');
+let portadaThumnails = document.querySelectorAll('.modal-gallery__thumnails');
+portadaThumnails = [...portadaThumnails];
+
+portadaThumnails.forEach(portadaThumnails => {
+    portadaThumnails.addEventListener('click', eventModal => {
+        modalImageContainer.style.backgroundImage = `url('../images/image-product-${eventModal.target.id.slice(-1)}.jpg')`;
+    });
+});
+
+
+//Cambiar imagen principal del modal desde las flechas en el modal.
+
+const modalNextBTN = document.querySelector('.modal-gallery__next');
+const modalPreviousBTN = document.querySelector('.modal-gallery__previous');
+
+modalNextBTN.addEventListener('click', () => {
+    changeNextImage(modalImageContainer);
+});
+
+modalPreviousBTN.addEventListener('click', () => {
+    changePreviousImage(modalImageContainer);
+});
+
+
+//Mostrar el navbar cuando se presiona el menú de hamburguesa
+
+const burguerMenu = document.querySelector('.header__menu'); 
+const modalNavbar = document.querySelector('.modal-navbar__background');
+const closeModalNavbar = document.querySelector('.modal-navbar__close');
+
+modalNavbar.style.display = 'none';
+
+burguerMenu.addEventListener('click', () => {
+    modalNavbar.style.display = 'block';
+});
+
+closeModalNavbar.addEventListener('click', () => {
+    modalNavbar.style.display = 'none';
 });
 
 
@@ -72,6 +159,7 @@ function updateProduct(){
 };
 
 function drawProductModal(){
+
     cartModalContainer.innerHTML = `
         <div class= "cart-modal__details-container">
         <img class="cart-modal__image" src="./images/image-product-1-thumbnail.jpg" alt="img-details">
@@ -87,3 +175,27 @@ function drawProductModal(){
     deleteProduct();
     updateProduct();
 };
+
+//Logica de cambiar imagenes con flechas.
+
+function changeNextImage(imgContainer) {
+    
+    if (imgIndex == 4) {
+        imgIndex = 1;
+    } else {
+        imgIndex++;
+    }
+    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`;
+};
+
+function changePreviousImage(imgContainer) {
+    
+    if (imgIndex == 1) {
+        imgIndex = 4;
+    } else {
+        imgIndex--;
+    }
+    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`;
+};
+
+
